33c21c4e15ab8de638d73d030dfd39a7072ce861
Ankit Coder <codeverseankit@gmail.com>
2026-01-26 21:26:15 +0530

    Add files via upload

---
+++ Helper header file/helper function test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "/sdcard/Coding.Cpp/PROJECT/Helper function/helper_function.h"
+using namespace std;
+int main()
+{
+    back_button();
+    clear_screen();
+    print_center("ankit");
+    loading_animation1("hello");
+    loading_bar();
+    print_text_with_animation("I am a full stack developer ", 50);
+
+    return 0;
+}
