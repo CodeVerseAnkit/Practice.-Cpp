7aa4cb27a9daa81c77a98735e3f60dfa0c207d3f
Ankit Coder <codeverseankit@gmail.com>
2026-01-26 21:33:46 +0530

    Add files via upload

---
+++ C language/string example.cxx
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+using namespace std;
+int main()
+{
+    char name[] = "ankit";
+
+    printf("%s", name);
+    printf("%d", strlen(name));
+    return 0;
+}
